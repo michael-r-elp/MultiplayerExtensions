29a2f539f194101814689952a9464e8520e928f9
EnderdracheLP <enderdrachelp@elp.quest>
2024-08-06 11:50:46 +0200

    Use the safe variant of the get env info method

--- MultiplayerExtensions/Patchers/MenuEnvironmentPatcher.cs
+++ MultiplayerExtensions/Patchers/MenuEnvironmentPatcher.cs
@@ -9,15 +9,18 @@ namespace MultiplayerExtensions.Patchers
     public class MenuEnvironmentPatcher : IAffinity
     {
         private readonly GameplaySetupViewController _gameplaySetup;
-        private readonly Config _config;
+        private readonly EnvironmentsListModel _environmentsListModel;
+		private readonly Config _config;
         private readonly SiraLog _logger;
 
         internal MenuEnvironmentPatcher(
             GameplaySetupViewController gameplaySetup,
+            EnvironmentsListModel environmentsListModel,
             Config config,
             SiraLog logger)
         {
             _gameplaySetup = gameplaySetup;
+            _environmentsListModel = environmentsListModel;
             _config = config;
             _logger = logger;
         }
@@ -34,20 +37,23 @@ namespace MultiplayerExtensions.Patchers
 
         [AffinityPrefix]
         [AffinityPatch(typeof(MultiplayerLevelScenesTransitionSetupDataSO), "Init")]
-        private void SetEnvironmentScene(IDifficultyBeatmap difficultyBeatmap, ref EnvironmentInfoSO ____multiplayerEnvironmentInfo)
+        private void SetEnvironmentScene(ref BeatmapKey beatmapKey, ref BeatmapLevel beatmapLevel, ref EnvironmentInfoSO ____multiplayerEnvironmentInfo)
         {
             if (!_config.SoloEnvironment)
                 return;
-
-            _originalEnvironmentInfo = ____multiplayerEnvironmentInfo;
-            ____multiplayerEnvironmentInfo = difficultyBeatmap.GetEnvironmentInfo();
+            EnvironmentName envName =
+	            beatmapLevel.GetEnvironmentName(beatmapKey.beatmapCharacteristic, beatmapKey.difficulty);
+            EnvironmentInfoSO? environmentInfo = _environmentsListModel.GetEnvironmentInfoBySerializedNameSafe(envName);
+			_originalEnvironmentInfo = ____multiplayerEnvironmentInfo;
+			____multiplayerEnvironmentInfo = environmentInfo;
+				____multiplayerEnvironmentInfo = environmentInfo;
             if (_gameplaySetup.environmentOverrideSettings.overrideEnvironments)
                 ____multiplayerEnvironmentInfo = _gameplaySetup.environmentOverrideSettings.GetOverrideEnvironmentInfoForType(____multiplayerEnvironmentInfo.environmentType);
         }
 
         [AffinityPostfix]
         [AffinityPatch(typeof(MultiplayerLevelScenesTransitionSetupDataSO), "Init")]
-        private void ResetEnvironmentScene(IDifficultyBeatmap difficultyBeatmap, ref EnvironmentInfoSO ____multiplayerEnvironmentInfo)
+        private void ResetEnvironmentScene(ref EnvironmentInfoSO ____multiplayerEnvironmentInfo)
         {
             if (_config.SoloEnvironment)
                 ____multiplayerEnvironmentInfo = _originalEnvironmentInfo;
